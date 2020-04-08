f92b3b99eb0a0a1bf5dc439284812a705b8b2dc1
Nikita Polar <qwers@me.com>
2020-04-09 00:46:19 +0300

    initial release

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+	//require_once 'app.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Монополия</title>
+  <meta name="description" content="Монополия">
+  <meta name="author" content="TheDiscordParty">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="fonts/fonts.css">
+</head>
+<body>
+	<div>
+		<div class="players" id="player-1">
+			<div class="avatar"></div>
+			<div class="nickname">Polar</div>
+			<div class="cash">1500</div>
+		</div>
+		<div class="players" id="player-2">
+			<div class="avatar"></div>
+			<div class="nickname">NZTgambler</div>
+			<div class="cash">1500</div>
+		</div>
+		<div class="players" id="player-3">
+			<div class="avatar"></div>
+			<div class="nickname">DevilRigoll</div>
+			<div class="cash">1500</div>
+		</div>
+	</div>
+	<div class="gamebox">
+		<div class="main-row">
+			<div class="main-field" id="card-0"></div>
+			<div class="vertical-field" id="card-1"></div>
+			<div class="vertical-field" id="card-2"></div>
+			<div class="vertical-field" id="card-3"></div>
+			<div class="vertical-field" id="card-4"></div>
+			<div class="vertical-field" id="card-5"></div>
+			<div class="vertical-field" id="card-6"></div>
+			<div class="vertical-field" id="card-7"></div>
+			<div class="vertical-field" id="card-8"></div>
+			<div class="vertical-field" id="card-9"></div>
+			<div class="main-field" id="card-10"></div>
+		</div>
+		<div class="second-row">
+			<div class="horizontal-field" id="card-39"></div>
+			<div class="horizontal-field" id="card-11"></div>
+		</div>
+		<div class="second-row">
+			<div class="horizontal-field" id="card-38"></div>
+			<div class="horizontal-field" id="card-12"></div>
+		</div>
+		<div class="second-row">
+			<div class="horizontal-field" id="card-37"></div>
+			<div class="horizontal-field" id="card-13"></div>
+		</div>
+		<div class="second-row">
+			<div class="horizontal-field" id="card-36"></div>
+			<div class="horizontal-field" id="card-14"></div>
+		</div>
+		<div class="second-row">
+			<div class="horizontal-field" id="card-35"></div>
+			<div class="horizontal-field" id="card-15"></div>
+		</div>
+		<div class="second-row">
+			<div class="horizontal-field" id="card-34"></div>
+			<div class="horizontal-field" id="card-16"></div>
+		</div>
+		<div class="second-row">
+			<div class="horizontal-field" id="card-33"></div>
+			<div class="horizontal-field" id="card-17"></div>
+		</div>
+		<div class="second-row">
+			<div class="horizontal-field" id="card-32"></div>
+			<div class="horizontal-field" id="card-18"></div>
+		</div>
+		<div class="second-row">
+			<div class="horizontal-field" id="card-31"></div>
+			<div class="horizontal-field" id="card-19"></div>
+		</div>
+		<div class="main-row">
+			<div class="main-field" id="card-30"></div>
+			<div class="vertical-field" id="card-29"></div>
+			<div class="vertical-field" id="card-28"></div>
+			<div class="vertical-field" id="card-27"></div>
+			<div class="vertical-field" id="card-26"></div>
+			<div class="vertical-field" id="card-25"></div>
+			<div class="vertical-field" id="card-24"></div>
+			<div class="vertical-field" id="card-23"></div>
+			<div class="vertical-field" id="card-22"></div>
+			<div class="vertical-field" id="card-21"></div>
+			<div class="main-field" id="card-20"></div>
+		</div>
+	</div>
+	<script src="js/scripts.js"></script>
+</body>
+</html>
